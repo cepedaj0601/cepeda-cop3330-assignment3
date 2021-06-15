fb38c82e8ddfa1c69104f8719e6230bec0d5db57
Justin Laptop <cepedaj0601@gmail.com>
2021-06-15 16:01:13 -0400

    Initial commit

---
+++ src/main/java/assignment3/ex41/base/Solution41.java
@@ -0,0 +1,50 @@
+package assignment3.ex41.base;
+/*
+ *  UCF COP3330 Summer 2021 ex41 Solution
+ *  Copyright 2021 Justin Cepeda
+ */
+/*
+Exercise 41 - Name Sorter
+Alphabetizing the contents of a file, or sorting its contents, is a great way to get comfortable manipulating a file in
+your program.
+
+Create a program that reads in the following list of names from a file called `exercise41_input.txt` and sorts the list
+alphabetically:
+
+Ling, Mai
+Johnson, Jim
+Zarnecki, Sabrina
+Jones, Chris
+Jones, Aaron
+Swift, Geoffrey
+Xiong, Fong
+Then print the sorted list to a file called `exercise41_output.txt` that looks like the following example output.
+
+Example Output
+Total of 7 names
+-----------------
+Ling, Mai
+Johnson, Jim
+Jones, Aaron
+Jones, Chris
+Swift, Geoffrey
+Xiong, Fong
+Zarnecki, Sabrina
+Constraint
+Don't hard-code the number of names.
+Challenges
+Implement this program by reading in the names from the user, one at a time, and printing out the sorted results to a
+file.
+Use the program to sort data from a large data set (e.g. census data) and use a profiler to analyze its performance.
+ */
+
+import java.util.Scanner;
+
+public class Solution41 {
+
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
