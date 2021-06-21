ec2741a2567bb08a650ede1751d8e1c16a806582
Justin Laptop <cepedaj0601@gmail.com>
2021-06-20 21:24:49 -0400

    commit

--- src/main/java/assignment3/ex43/base/Html.java
+++ src/main/java/assignment3/ex43/base/Html.java
@@ -2,5 +2,14 @@ package assignment3.ex43.base;
 
 //contains function used to create html file
 public class Html {
+
     //creates html file
+    public static void htmlMaker(){
+        //take in html information
+        //add name
+        //add author
+        //add javascript
+        //add css
+        //return output for testing
+    }
 }
--- src/main/java/assignment3/ex43/base/InfoGetters.java
+++ src/main/java/assignment3/ex43/base/InfoGetters.java
@@ -2,8 +2,32 @@ package assignment3.ex43.base;
 
 //contains functions used to get user input about website creation
 public class InfoGetters {
+
     //function to get the name
+    public static void getName(){
+        //prompt input
+        //scan and store input
+        //return input
+    }
+
     //function to get the author
+    public static void getAuthor(){
+        //prompt input
+        //scan and store input
+        //return input
+    }
+
     //function to know if JavaScript is made
+    public static void getJava(){
+        //prompt input
+        //scan and store input
+        //return input
+    }
+
     //function to know if Css is made
+    public static void getCss(){
+        //prompt input
+        //scan and store input
+        //return input
+    }
 }
